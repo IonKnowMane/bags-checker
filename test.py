c584e2addd31a9da097d26638fece8199711295b
IonKnowMane <you@example.com>
2025-12-28 21:30:43 +0100

    more work

--- test.py
+++ test.py
@@ -1,18 +1,27 @@
 from requests import get
-from json import loads
+from json import loads, dump
 
-response = get("https://api2.bags.fm/api/v1/token-launch/top-tokens/lifetime-fees")
-data: dict[str, str | dict] = loads(response.text)
+def main() -> None:
+    response = get("https://api2.bags.fm/api/v1/token-launch/top-tokens/lifetime-fees")
+    data: dict[str, str | dict] = loads(response.text)
+    results: dict[str, dict[str, str | float | int]] = {}
 
-if data["success"]:
-    for i, t in enumerate(data["response"]):
-        total_fees = float(t.get("lifetimeFees", 0))
-        if total_fees > 0:
-            print(f"{i}) {t["tokenInfo"]["symbol"]} - total fees: {round(total_fees / (10 ** 9), 2)} SOL")
-            print(t["token"])
-            for c in t.get("creators", []):
-                print(f"{c['providerUsername'] or "missing"} | {c["royaltyBps"] / 100} | {round(float(c["totalClaimed"]) / (10 ** 9), 2)} SOL")
-                total_fees -= float(c["totalClaimed"])
-            unclaimed = round(total_fees / (10 ** 9), 2)
-            print(f"{unclaimed} SOL available!")
-        print("\n")
+    if data["success"]:
+        for i, t in enumerate(data["response"]):
+            info: dict[str, str | float | int] = {}
+            total_fees = float(t.get("lifetimeFees", 0))
+            if total_fees > 0:
+                print(f"{i}) {t["tokenInfo"]["symbol"]} - total fees: {round(total_fees / (10 ** 9), 2)} SOL")
+                print(t["token"])
+                for c in t.get("creators", []):
+                    print(f"{c['providerUsername'] or "missing"} | {c["royaltyBps"] / 100} | {round(float(c["totalClaimed"]) / (10 ** 9), 2)} SOL")
+                    total_fees -= float(c["totalClaimed"])
+                unclaimed = round(total_fees / (10 ** 9), 2)
+                print(f"{unclaimed} SOL available!")
+            print("\n")
+
+    with open(".\\exports\\bags_top_100.json", "w", encoding="utf-8") as f:
+        dump(data, f, indent=4, ensure_ascii=False)
+
+if __name__ == "__main__":
+    main()
